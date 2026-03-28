8c3512ab93058ee2473a2e1f591fa02f751006ca
LindaFPayeras <lindafpayeraso@gmail.com>
2026-03-28 19:33:11 +0100

    Interfaz chat

--- viewPatient.py
+++ viewPatient.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
-def viewPatient():
-    st.title("Psico App")
-    if "message" not in st.session_state:
-        st.session_state.messages = []
-    
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+st.title("Psico App")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    if prompt := st.chat_input("Type your message here..."):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("How are you feeling today?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    
 
     
